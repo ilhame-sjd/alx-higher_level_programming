95c81067091ff29d9345c2dad975923b6a531593
“ilhame” <ilhame.sajid@gmail.com>
2023-12-04 04:10:53 -0800

    Divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def divisible_by_2(my_list=[]):
     if my_list is None:
         return None
@@ -7,8 +8,9 @@ def divisible_by_2(my_list=[]):
         if (i % 2) == 0:
             list_divs.append(True)
         else:
-            list_disv.append(False)
-    return list_divs        
+            list_divs.append(False)
+    return list_divs
+        
 
 
 
