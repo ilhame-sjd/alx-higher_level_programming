4a074bb4ab1d982ddc1c294fe0fed6f5225b184e
“ilhame” <ilhame.sajid@gmail.com>
2023-12-04 03:48:02 -0800

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) < 1:
+        return None
+    my_list_copy = my_list.copy()
+    my_list_copy.sort()
+    return my_list_copy[-1]
+   
