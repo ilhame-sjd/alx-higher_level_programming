1b988ab97ad4e16071e882044becb691f3190b06
“ilhame” <ilhame.sajid@gmail.com>
2023-12-04 02:40:47 -0800

    List_of_matrix = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for submatrix in matrix:
         if len(submatrix) == 0:
             print()
-        for i in range(submatrix):
+        for i in range(len(submatrix)):
             print("{:d}".format(submatrix[i]),
-                  end="\n" if i is len(submatrix)-1 else " ") 
+                  end="\n" if i == len(submatrix)-1 else " ")
+
